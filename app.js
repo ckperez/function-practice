a75238daf5fc57f6a2aad993769f5fd2ed4af907
Chris Perez <chris.k.perez@gmail.com>
2016-03-18 11:21:34 -0700

    Added problem 4 with array argument

--- app.js
+++ app.js
@@ -19,3 +19,13 @@ function sumAndMultiply(firstSAM, secondSAM, thirdSAM) {
   console.log('The numbers ' + firstSAM + ' and ' + secondSAM + ' and ' + thirdSAM + ' have a product of ' + endProduct + '.');
   return [endSum, endProduct];
 }
+
+function sumArray(p4Array) {
+  var total = 0;
+  for (var i = 0; i < p4Array.length; i++) {
+    total = total + p4Array[i];
+  }
+  console.table(p4Array);
+  console.log(p4Array + ' was passed as an array of numbers, and ' + total + ' is their sum.');
+  return total;
+}
